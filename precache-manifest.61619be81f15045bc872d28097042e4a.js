f0084b34d6ec2de367278b351f69d894786fe9b0
Emil Jan <anoldwebsite@gmail.com>
2020-04-04 20:17:01 +0100

    Updates

---
+++ precache-manifest.61619be81f15045bc872d28097042e4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2270b3723bf9df05d715ffd7014c959",
+    "url": "/third-react-app/index.html"
+  },
+  {
+    "revision": "edb9fe2c4f9ea8bafa55",
+    "url": "/third-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6fb0faa837559f7e9f4b",
+    "url": "/third-react-app/static/js/2.085a0cd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/third-react-app/static/js/2.085a0cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb9fe2c4f9ea8bafa55",
+    "url": "/third-react-app/static/js/main.781977a3.chunk.js"
+  },
+  {
+    "revision": "cc4700cdd1531f44c58c",
+    "url": "/third-react-app/static/js/runtime-main.e76b9650.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/third-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
