ec4371bb27d77ecf6d91a51a344c7ba85506f968
Emil Jan <anoldwebsite@gmail.com>
2020-04-04 20:14:50 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sum: 33 + this.props.increment //Adding the prop increment into our sum
+    }
+  }
+  /*
+  If you don't need to use the "this" like we have above then you don't need to pass props to 
+  constructor and super and you even don't need to write so much code. You don't need to write
+  the constructor. An alternative way to do this is:
+    state = {
+      sum: 33 
+    }
+  */
+  handleClick = () => {
+    this.setState((prevState, prevProps) => {
+      return {
+        sum: prevState.sum + prevProps.increment
+      }
+    },
+      () => console.log(this.state.sum)//This is the callback function which is passed as a second argument to the method setState
+    );
+
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>{this.state.sum}</h2>
+          <button onClick={this.handleClick}>Update the state</button>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
